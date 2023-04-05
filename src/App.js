d0b8d256352f739ff916b983bf252f54216d325b
sadaftarannum96 <101640769+sadaftarannum96@users.noreply.github.com>
2023-04-05 15:47:45 +0530

    Add files via upload
    react hook form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+// import './App.css';
+import SignupReacthookF from './components/SignupReacthookF';
+
+function App() {
+  return (
+    <div className="App">
+      <SignupReacthookF/>
+    </div>
+  );
+}
+
+export default App;
